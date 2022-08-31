f9f289d653be978407e524c59a896c4835668b2c
santiagoposadag <santiago90dell@gmail.com>
2022-08-31 18:40:24 -0500

    Email changed and phone number

--- src/js/general.js
+++ src/js/general.js
@@ -1,11 +1,21 @@
 "use strict";
-const listAnchors = document.querySelectorAll('.go_to_section');
-console.log(listAnchors);
-listAnchors.forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        // const singleAnchorId:string | null  = anchor.getAttribute('href');
-        // console.log(singleAnchorId);
-        // const linkedTo = document.querySelector(singleAnchorId)
+// const listAnchors = document.querySelectorAll('.go_to_section') as NodeListOf<HTMLAnchorElement>
+// console.log(listAnchors);
+// listAnchors.forEach(anchor => {
+//   anchor.addEventListener('click', function (e) {
+//       e.preventDefault();
+//       // const singleAnchorId:string | null  = anchor.getAttribute('href');
+//       // console.log(singleAnchorId);
+//       // const linkedTo = document.querySelector(singleAnchorId)
+//   });
+// });
+const link = document.querySelector('.go_to_section_contacto');
+link.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log('desde el link hacia el contacot');
+    const section = document.querySelector('#target4');
+    console.log(section);
+    section.scrollIntoView({
+        behavior: 'smooth'
     });
 });
--- src/ts/general.ts
+++ src/ts/general.ts
@@ -1,14 +1,31 @@
 
-const listAnchors = document.querySelectorAll('.go_to_section') as NodeListOf<HTMLAnchorElement>
-console.log(listAnchors);
+// const listAnchors = document.querySelectorAll('.go_to_section') as NodeListOf<HTMLAnchorElement>
+// console.log(listAnchors);
 
-listAnchors.forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-      e.preventDefault();
+// listAnchors.forEach(anchor => {
+//   anchor.addEventListener('click', function (e) {
+//       e.preventDefault();
 
-      // const singleAnchorId:string | null  = anchor.getAttribute('href');
-      // console.log(singleAnchorId);
+//       // const singleAnchorId:string | null  = anchor.getAttribute('href');
+//       // console.log(singleAnchorId);
 
-      // const linkedTo = document.querySelector(singleAnchorId)
-  });
-});
+//       // const linkedTo = document.querySelector(singleAnchorId)
+//   });
+// });
+
+
+const link = document.querySelector('.go_to_section_contacto') as HTMLAnchorElement;
+
+link.addEventListener('click', (e) => {e.preventDefault()
+  console.log('desde el link hacia el contacot');
+  
+  const section = document.querySelector('#target4') as HTMLHeadingElement;
+  console.log(section);
+  
+  section.scrollIntoView({
+    behavior:'smooth'
+  })
+})
+
+
+
